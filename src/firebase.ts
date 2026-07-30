e680a88add3e7d7bf1ef6bec97461e2a42daf9b3
kolisettyadarsh2003-code <kolisettyadarsh2003@gmail.com>
2026-07-30 21:50:33 +0530

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,1 +1,18 @@
 
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyALvlpDsazV8STwC5RotwEgFBzJawoxxW0",
+  authDomain: "parasakthi-bakery.firebaseapp.com",
+  projectId: "parasakthi-bakery",
+  storageBucket: "parasakthi-bakery.firebasestorage.app",
+  messagingSenderId: "298622831699",
+  appId: "1:298622831699:web:7851908c568157ab110aa6"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+export const storage = getStorage(app);
