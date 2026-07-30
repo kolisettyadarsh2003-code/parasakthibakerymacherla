bbd5b71eef3defa2121929dafe7052a9a7975117
kolisettyadarsh2003-code <kolisettyadarsh2003@gmail.com>
2026-07-30 21:54:20 +0530

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,18 +1,21 @@
-
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyALvlpDsazV8STwC5RotwEgFBzJawoxxW0",
   authDomain: "parasakthi-bakery.firebaseapp.com",
   projectId: "parasakthi-bakery",
   storageBucket: "parasakthi-bakery.firebasestorage.app",
   messagingSenderId: "298622831699",
-  appId: "1:298622831699:web:7851908c568157ab110aa6"
+  appId: "1:298622831699:web:54fd746b6bcece52110aa6",
+  measurementId: "G-3QKDJG1PH8"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-export const db = getFirestore(app);
-export const storage = getStorage(app);
+const analytics = getAnalytics(app);
